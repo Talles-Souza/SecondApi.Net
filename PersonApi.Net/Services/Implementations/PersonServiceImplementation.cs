ca61fe96447c62da50731079b71e1fef7b25443e
Talles Dias <tallesdelfino1@gmail.com>
2022-09-21 15:57:30 -0300

    implementando a interface

---
+++ PersonApi.Net/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,42 @@
+﻿using PersonApi.Net.Models;
+
+namespace PersonApi.Net.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        
+
+        public Person Create(Person person)
+        {
+            return person;    
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = null;
+            return people;
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Talles",
+                LastName = "Souza",
+                Address = "Petrópolis",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
