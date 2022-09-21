ab3cce9dbaf353cf6281981954ba918663bacba2
Talles Dias <tallesdelfino1@gmail.com>
2022-09-21 16:27:04 -0300

    adicionando a injeção de dependencia

--- PersonApi.Net/Program.cs
+++ PersonApi.Net/Program.cs
@@ -1,8 +1,13 @@
+using PersonApi.Net.Services;
+using PersonApi.Net.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//injeção de dependencia
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>(); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
